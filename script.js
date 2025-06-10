4cd95a75c69c37d187d0533c1ceb34bb1dcbb67f
CarlossMartinez <carkevinmartinez@gmail.com>
2025-06-10 13:33:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("spinner").style.display = "none";
 });
+document.onload = function() {
+    alert("Esta web esta principalmente enfocada en usuarios de telefono movil, por lo que si la estas viendo desde un ordenador, te recomendamos que la veas desde el movil. Gracias.");
+}
